c6e67d8dd618e7405e5a0bc651dbc5da35d57ded
Steven Seiden <stevenrovello@gmail.com>
2019-01-14 16:03:00 -0600

    Finished the game engine

--- main.cpp
+++ main.cpp
@@ -21,37 +21,40 @@ void adjust(char userInput){
     int moveVert;
     int startPointH = 0; //The point on the board at which points are looked at
     int startPointV = 0;
+    int endPointV = 4;
     if(userInput == 'w'){
-        moveHoriz = 1;
-        moveVert = 0;
-    }else if(userInput == 's'){
-        moveHoriz = -1;
-        moveVert = 0;
-        startPointH = 1;
-    }else if(userInput == 'a'){
         moveHoriz = 0;
         moveVert = 1;
-    }else if(userInput == 'd'){
+    }else if(userInput == 's'){
         moveHoriz = 0;
         moveVert = -1;
         startPointH = 1;
+    }else if(userInput == 'a'){
+        moveHoriz = 1;
+        moveVert = 0;
+        endPointV = 3;
+    }else if(userInput == 'd'){
+        moveHoriz = -1;
+        moveVert = 0;
+        startPointV = 1;
     }
     else{
         moveHoriz = 0;
         moveVert = 0;
     } for (int i = 0; i < 4; i++) { //Running for the maximum amount of times necessary to adjust everything
                 for(int vert = startPointH; vert < 4; vert++) {
-                    for (int horiz = startPointV; horiz < 4; horiz++) {
-                        if(board[vert][horiz] == 0 && board[vert+moveHoriz][horiz+moveVert] != 0){
-                            board[vert][horiz] = board[vert+moveHoriz][horiz+moveVert];
-                            board[vert+moveHoriz][horiz+moveVert] = 0;
-                            printf("Moving (%d,%d) to (%d,%d).\n",(vert+moveHoriz),(horiz+moveVert),vert,horiz);
+                    for (int horiz = startPointV; horiz < endPointV; horiz++) {
+                        if(board[vert][horiz] == 0 && board[vert+moveVert][horiz+moveHoriz] != 0){
+                            board[vert][horiz] = board[vert+moveVert][horiz+moveHoriz];
+                            board[vert+moveVert][horiz+moveHoriz] = 0;
+                            printf("Moving (%d,%d) to (%d,%d).\n",(vert+moveVert),(horiz+moveHoriz),vert,horiz);
                         }
 
-                        if(board[vert][horiz] == board[vert+moveHoriz][horiz+moveVert] && board[vert][horiz]!=0){
+                        if(board[vert][horiz] == board[vert+moveVert][horiz+moveHoriz] && board[vert][horiz]!=0){
                             board[vert][horiz] = (board[vert][horiz]*2);
-                            board[vert+moveHoriz][horiz] = 0;
-                            printf("Adding (%d,%d) to (%d,%d).\n",(vert+1),(horiz+moveVert),vert,horiz);
+                            //The vert and horiz values are meant to be swapped here.
+                            board[vert+moveVert][horiz+moveHoriz] = 0;
+                            printf("Adding (%d,%d) to (%d,%d).\n",(vert+moveVert),(horiz+moveHoriz),vert,horiz);
                         }
                     }
 
@@ -74,12 +77,18 @@ void PrintBoard()
     );
 }
 
-int tileGen(){
+void tileGen(){
+    int pos2x = (rand() % 3 + 0);
+    int pos2y = (rand() % 3 + 0);
+    while(board[pos2x][pos2y] != 0){
+        pos2x = (rand() % 3 + 0);
+        pos2y = (rand() % 3 + 0);
+    }
     int newTile = randomNum();
-    if(newTile >= 7){
-        return 4;
+    if(newTile >= 9){
+        board[pos2x][pos2y] = 4;
     }else{
-        return 2;
+        board[pos2x][pos2y] =  2;
     }
 }
 
@@ -88,34 +97,19 @@ int main() {
 
     int array [3][3] = {0};
 
-
-    /*board[(rand() % 3 + 0)][(rand() % 3 + 0)] = tileGen();
-    int pos2x = (rand() % 3 + 0);
-    int pos2y = (rand() % 3 + 0);
-    while(board[pos2x][pos2y] != 0){
-        pos2x = (rand() % 3 + 0);
-        pos2y = (rand() % 3 + 0);
-    }
-
-    board[pos2x][pos2y] = tileGen();*/
-
-
-    board[2][0] = 2;
-    board[0][1] = 2;
-    board[0][2] = 2;
-    board[1][2] = 2;
-    board[2][3] = 2;
-
+    tileGen();
+    tileGen();
 
     Boolean playing = true;
 
-    int userInput;
+    PrintBoard();
+    char userInput;
     while (playing) {
-        PrintBoard();
+
         scanf(" %c", &userInput);
             adjust(userInput);
-
-        PrintBoard();
+            tileGen();
+            PrintBoard();
     }
 
     return 0;
