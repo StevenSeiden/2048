3bce2e4820aebb245fcf503df45bd293dc1b3435
Steven Seiden <stevenrovello@gmail.com>
2019-01-18 21:46:46 -0600

    Added column spacing and score counter

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 #include <math.h>
 #include <time.h>
 #include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
 int board[4][4];
+int score = 1;
 
 int randomNum(){
     int randomNumber;
@@ -42,24 +46,25 @@ void adjust(char userInput){
         moveHoriz = 0;
         moveVert = 0;
     } for (int i = 0; i < 4; i++) { //Running for the maximum amount of times necessary to adjust everything
-                for(int vert = startPointH; vert < 4; vert++) {
-                    for (int horiz = startPointV; horiz < endPointV; horiz++) {
-                        if(board[vert][horiz] == 0 && board[vert+moveVert][horiz+moveHoriz] != 0){
-                            board[vert][horiz] = board[vert+moveVert][horiz+moveHoriz];
-                            board[vert+moveVert][horiz+moveHoriz] = 0;
-                            printf("Moving (%d,%d) to (%d,%d).\n",(vert+moveVert),(horiz+moveHoriz),vert,horiz);
-                        }
-
-                        if(board[vert][horiz] == board[vert+moveVert][horiz+moveHoriz] && board[vert][horiz]!=0){
-                            board[vert][horiz] = (board[vert][horiz]*2);
-                            //The vert and horiz values are meant to be swapped here.
-                            board[vert+moveVert][horiz+moveHoriz] = 0;
-                            printf("Adding (%d,%d) to (%d,%d).\n",(vert+moveVert),(horiz+moveHoriz),vert,horiz);
-                        }
-                    }
-
+        for(int vert = startPointH; vert < 4; vert++) {
+            for (int horiz = startPointV; horiz < endPointV; horiz++) {
+                if(board[vert][horiz] == 0 && board[vert+moveVert][horiz+moveHoriz] != 0){
+                    board[vert][horiz] = board[vert+moveVert][horiz+moveHoriz];
+                    board[vert+moveVert][horiz+moveHoriz] = 0;
+                    printf("Moving (%d,%d) to (%d,%d).\n",(vert+moveVert),(horiz+moveHoriz),vert,horiz);
                 }
+
+                if(board[vert][horiz] == board[vert+moveVert][horiz+moveHoriz] && board[vert][horiz]!=0){
+                    board[vert][horiz] = (board[vert][horiz]*2);
+                    //The vert and horiz values are meant to be swapped here.
+                    board[vert+moveVert][horiz+moveHoriz] = 0;
+                    printf("Adding (%d,%d) to (%d,%d).\n",(vert+moveVert),(horiz+moveHoriz),vert,horiz);
+                    score = score + board[vert][horiz];
+                }
+            }
+
         }
+    }
 
 }
 
@@ -69,12 +74,18 @@ void PrintBoard()
     for (n = 0; n < 10; n++)
         printf( "\n" );
 
-    printf("%d  %d  %d  %d \n%d  %d  %d  %d \n%d  %d  %d  %d \n%d  %d  %d  %d \n",
-           board[0][0],board[0][1],board[0][2],board[0][3],
-           board[1][0],board[1][1],board[1][2],board[1][3],
-           board[2][0],board[2][1],board[2][2],board[2][3],
-           board[3][0],board[3][1],board[3][2],board[3][3]
-    );
+    cout<<("Score: ")<<score-1<<endl;
+    cout<<setfill(' ')<<setw(4)<<board[0][0]<<setfill(' ')<<setw(4)<<board[0][1]
+        <<setfill(' ')<<setw(4)<<board[0][2]<<setfill(' ')<<setw(4)<<board[0][3]<<endl;
+
+    cout<<setfill(' ')<<setw(4)<<board[1][0]<<setfill(' ')<<setw(4)<<board[1][1]
+        <<setfill(' ')<<setw(4)<<board[1][2]<<setfill(' ')<<setw(4)<<board[1][3]<<endl;
+
+    cout<<setfill(' ')<<setw(4)<<board[2][0]<<setfill(' ')<<setw(4)<<board[2][1]
+        <<setfill(' ')<<setw(4)<<board[2][2]<<setfill(' ')<<setw(4)<<board[2][3]<<endl;
+
+    cout<<setfill(' ')<<setw(4)<<board[3][0]<<setfill(' ')<<setw(4)<<board[3][1]
+        <<setfill(' ')<<setw(4)<<board[3][2]<<setfill(' ')<<setw(4)<<board[3][3]<<endl;
 }
 
 void tileGen(){
@@ -87,8 +98,10 @@ void tileGen(){
     int newTile = randomNum();
     if(newTile >= 9){
         board[pos2x][pos2y] = 4;
+        cout<<("Adding a tile of ")<<4<<(" at position ")<<pos2x<<(", ")<<pos2y<<endl;
     }else{
         board[pos2x][pos2y] =  2;
+        cout<<("Adding a tile of ")<<2<<(" at position ")<<pos2x<<(", ")<<pos2y<<endl;
     }
 }
 
@@ -105,14 +118,15 @@ int main() {
     PrintBoard();
     char userInput;
     while (playing) {
+
         scanf(" %c", &userInput);
         if(userInput=='x') {
             return 0;
-        }else if(userInput=='w' || userInput=='a' || userInput=='s' || userInput=='d'){
+        }else if(userInput=='w' || userInput=='a' || userInput=='s' || userInput=='d') {
             adjust(userInput);
+            tileGen();
+            PrintBoard();
         }
-        tileGen();
-        PrintBoard();
     }
 
     return 0;
