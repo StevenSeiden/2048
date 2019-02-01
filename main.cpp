738326571b639c8cb76cde6970f2467393dd42d9
Steven Seiden <stevenrovello@gmail.com>
2019-02-01 15:43:37 -0600

    Fixed error in moving up, checks if move was valid before adding piece, and checks for full boards

--- main.cpp
+++ main.cpp
@@ -12,10 +12,12 @@ using namespace std;
 std::string reset = "\033[0m";
 std::string blue = "\u001b[34;5;1m";
 
+int boardSize = 4;
 int board[4][4];
 int score = 0;
 string highScore;
 bool reachedGoal;
+bool changesMade;
 
 int randomNum() {
     int randomNumber;
@@ -32,6 +34,7 @@ void moveTiles(int vert, int horiz, int moveVert, int moveHoriz) {
         board[vert][horiz] = board[vert + moveVert][horiz + moveHoriz];
         board[vert + moveVert][horiz + moveHoriz] = 0;
         printf("Moving (%d,%d) to (%d,%d).\n", (vert + moveVert), (horiz + moveHoriz), vert, horiz);
+        changesMade = true;
     }
 }
 
@@ -45,29 +48,50 @@ void combineTiles(int vert, int horiz, int moveVert, int moveHoriz) {
         if(board[vert][horiz] == 16){
             reachedGoal = true;
         }
+        changesMade = true;
     }
 }
 
-void adjust(char userInput) {
-    for (int i = 0; i < 4; i++) { //Running for the maximum amount of times necessary to adjust everything
-        if (userInput == 's') {
-            for (int vert = 3; vert > 0; vert--) {
-                for (int horiz = 0; horiz < 3; horiz++) {
-                    moveTiles(vert, horiz, -1, 0);
-                    combineTiles(vert, horiz, -1, 0);
-                }
+//This checks to see if the board is full, and returns true if full
+Boolean checkFull(){
+    Boolean isFull = false;
+    for(int i = 0; i<(boardSize-1); i = i + 2){
+        for(int j = 0; i<(boardSize-1); i = i + 2){
+            if(board[i][j] != 0 && board[i][j] != board[i+1][j] &&
+            board[i][j] != board[i][j+1] && board[i+1][j] != 0 &&
+            board[i][j+1] != 0){
+                isFull = true;
+            }else{
+                return false; //At least one location is open
             }
+        }
+    }
+    return isFull;
+}
+
+void adjust(char userInput) {
+    changesMade = false;
+    for (int i = 0; i < 4; i++) { //Running for the maximum amount of times necessary to adjust everything
+        if (userInput == 's' || userInput == 'd') {
+                for (int vert = 3; vert > 0; vert--) {
+                    if (userInput == 's') {
+                        for (int horiz = 0; horiz < 4; horiz++) {
+                            moveTiles(vert, horiz, -1, 0);
+                            combineTiles(vert, horiz, -1, 0);
+                        }
+                    } else {
+                        for (int horiz = 3; horiz > 0; horiz--) {
+                            moveTiles(vert, horiz, 0, -1);
+                            combineTiles(vert, horiz, 0, -1);
+                        }
+                    }
+                }
         } else {
             for (int vert = 0; vert < 3; vert++) {
                 if (userInput == 'w') {
                     for (int horiz = 0; horiz < 4; horiz++) {
                         moveTiles(vert, horiz, 1, 0);
                         combineTiles(vert, horiz, 1, 0);
-                    }
-                }else if (userInput == 'd') {
-                    for (int horiz = 3; horiz > 0; horiz--) {
-                        moveTiles(vert, horiz, 0, -1);
-                        combineTiles(vert, horiz, 0, -1);
                     }
                 } else {
                     for (int horiz = 0; horiz < 3; horiz++) {
@@ -80,7 +104,7 @@ void adjust(char userInput) {
     }
 }
 
-void PrintBoard() {
+void printBoard() {
     int n;
     for (n = 0; n < 10; n++)
         printf("\n");
@@ -137,9 +161,9 @@ int main() {
     ifstream scoreSheet ("scoresheet.txt");
     if (scoreSheet.is_open())
     {
-        while ( getline (scoreSheet,highScore) )
+        while ( getline(scoreSheet,highScore))
         {
-            cout << highScore << '\n';
+            cout << "High Score: " << highScore << '\n';
         }
         scoreSheet.close();
     }
@@ -162,7 +186,7 @@ int main() {
 
     Boolean playing = true;
 
-    PrintBoard();
+    printBoard();
     char userInput;
     while (playing) {
 
@@ -171,8 +195,14 @@ int main() {
             return 0;
         } else if (userInput == 'w' || userInput == 'a' || userInput == 's' || userInput == 'd') {
             adjust(userInput);
-            tileGen();
-            PrintBoard();
+            if(changesMade){
+                tileGen();
+                changesMade = false;
+            }
+            printBoard();
+            if(checkFull()){
+                cout << "Game Over" << endl;
+            }
         }
     }
 
